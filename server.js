e6b3b24517f4a83a4359404a3fc788b35c207d30
suraj401 <83824847+suraj401@users.noreply.github.com>
2022-08-30 11:22:19 +0530

    Update server.js
    mada di added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World....Namskar');
+  res.send('Hello World....Namskar..Jai mata di');
 });
 
 app.listen(PORT, HOST);
