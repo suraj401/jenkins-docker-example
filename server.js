1f558bc37d7a8b703d958eaacb614c92f5f3fd8e
suraj401 <83824847+suraj401@users.noreply.github.com>
2022-08-30 11:24:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World....Namskar..Jai mata di');
+  res.send('Hello World....Namskar..Jai shriram');
 });
 
 app.listen(PORT, HOST);
